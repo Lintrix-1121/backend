179387073c0eb1bdba8ffa472a1b47262d8e5bd0
lintrix <lintrix@Lintrix.lintrix.com>
2026-05-28 07:44:09 +0300

    Cors blocking resolution

--- server.js
+++ server.js
@@ -53,7 +53,12 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:2090'],
+  origin: [
+    'https://logiphix.tech',
+    'https://www.logiphix.tech',
+    'https://api.logiphix.tech',
+    'http://localhost:5173'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Session-ID', 'X-User-ID', 'Accept', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Cache-Control', 'Pragma', 'If-Modified-Since'],
